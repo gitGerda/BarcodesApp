4ac14901b2d24ea09f851722a6c6ab66f71c237f
gitGerda <42517716+gitGerda@users.noreply.github.com>
2021-09-30 07:58:39 +0300

    300921_0758

---
+++ TestApplication/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Euro Plus">
+//     Copyright © Euro Plus 2014.
+// </copyright>
+// <summary>This is the MainWindow Window.</summary>
+//-----------------------------------------------------------------------
+namespace NiceLabel.SDK.DemoApp
+{
+    using System.Windows;
+    using System.IO.Ports;
+
+    /// <summary>
+    /// Interaction logic for MainWindow Window.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+
+        SerialPort _serialPort;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            this.DataContext = new MainWindowViewModel();
+
+            //string f = Properties.Settings.Default.ComPort;
+
+            //Properties.Settings.Default.ComPort = "Test2";
+            //Properties.Settings.Default.Save();
+
+            //f= Properties.Settings.Default.ComPort;
+        }
+    }
+}
